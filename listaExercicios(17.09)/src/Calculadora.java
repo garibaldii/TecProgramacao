b3b200ea4478d1aa37f446a251747a8b611b0f1d
Matheus Garibaldi <125389052+garibaldii@users.noreply.github.com>
2023-09-12 17:24:43 -0300

    Lógica ok, porém existem formas mais eficientes que facilitem a manutenção

--- listaExercicios(17.09)/src/Calculadora.java
+++ listaExercicios(17.09)/src/Calculadora.java
@@ -15,7 +15,6 @@ public class Calculadora {
 
         this.resultado = (valorA + valorB);
 
-        System.out.println(this.resultado);
     }
 
     public void sub(double valorA, double valorB) {
@@ -27,7 +26,6 @@ public class Calculadora {
 
         this.resultado = (valorA - valorB);
 
-        System.out.println(this.resultado);
     }
 
     public void mult(double valorA, double valorB) {
@@ -39,7 +37,6 @@ public class Calculadora {
 
         this.resultado = (valorA * valorB);
 
-        System.out.println(this.resultado);
     }
 
     public void div(double valorA, double valorB) {
@@ -52,7 +49,6 @@ public class Calculadora {
             this.valorB = valorB;
 
             this.resultado = (valorA / valorB);
-            System.out.println(this.resultado);
 
         } else {
             System.out.println("Insira um divisor válido");
@@ -62,73 +58,89 @@ public class Calculadora {
 
     public String getResultadoString() {
 
-        this.resultadoMsg = this.valorA + " " + this.operador + " " + this.valorB + " = " + this.resultado;
+        if (this.operador == null) {
 
-        return this.resultadoMsg;
+            if (valorB == 0) { 
+                // uma forma de separar a exibição do resultado dos dois métodos.
+                // para método decimal => binário, usa-se o valorA como referência
+                // e o contrário também acontece para binário => decimal. 
+                this.resultadoMsg = this.valorA + " em decimal é igual a " + this.resultado + " em binário";
+            }
+            else{
+                this.resultadoMsg = this.valorB + " em binário é igual a " + this.resultado + " em decimal";
+            }
+        }
+
+        else {
+            // nesse caso, só irá exibir esse formato se o método requisitado por um usuário tiver uma operacao
+            // matemática direta. 
+            this.resultadoMsg = this.valorA + " " + this.operador + " " + this.valorB + " = " + this.resultado;
+        }
+        return resultadoMsg;
+    }
+
+    public double convertDecToBin(int decimal) {
+        this.operador = null;
+        this.valorA = decimal;
+        this.valorB = 0;
+        double resultadoBinario = 0;// inicializei a variável para poder somar na linha 81
+        double resto;
+        double posicao = 1;
+        while (decimal > 0) {
+            resto = decimal % 2;
+            decimal = decimal / 2;
+            resultadoBinario += resto * posicao;
+            posicao *= 10; // aqui vai fazer a multiplicacao para posicionar os números binários com suas
+                           // respectivas casas.
+                           // toda vez que o resto for igual a 0, o binário vai manter seu valor, se não,
+                           // ele irá ser multiplicado pela próxima casa
+                           // do algarismo posicao, por exemplo se o resto for igual a 1 e a posicao já
+            // estiver na terceira leva (1000), o resto será dado
+            // por 1 + (1 * 1000) =1001 e assim sucessivamente para os próximos valores do
+            // resto.
+
+        }
+        this.resultado = resultadoBinario;
+        return resultadoBinario;
+
     }
 
     public double getResultadoValor() {
         return this.resultado;
     }
 
-    public int convertDecToBin(int decimal) {
-        int resto;
-        int binario = 0;//inicializei a variável para poder somar na linha 81
-        int posicao = 1;
-        while (decimal > 0) {
-            resto = decimal % 2;
-            decimal = decimal / 2;
-            binario += resto * posicao;
-            posicao *= 10; //aqui vai fazer a multiplicacao para posicionar os números binários com suas respectivas casas.
-            //toda vez que o resto for igual a 0, o binário vai manter seu valor, se não, ele irá ser multiplicado pela próxima casa
-            // do algarismo posicao, por exemplo se o resto for igual a 1 e a posicao já estiver na terceira leva (1000), o resto será dado
-            //por 1 + (1 * 1000) =1001 e assim sucessivamente para os próximos valores do resto. 
-
-        }
-        return binario;
-        
-    }
-
     public int convertBinToDec(String binario) {
+        this.operador = null;
         int decimal = 0;
-    
-        for (int i = 0; i < binario.length(); i++) {
+        this.valorB = Double.parseDouble(binario);
+
+        for (int i = binario.length() - 1; i >= 0; i--) {
 
             char digito = binario.charAt(i);
-    
+
             switch (digito) {
                 case '0':
                     // Se for '0', não fazemos nada, pois o valor já é 0.
                     break;
-    
+
                 case '1':
                     // Se for '1', adicionamos 2 elevado à posição atual à soma.
-                    decimal = (int) (decimal + Math.pow(2, i));
+                    decimal = (int) (decimal + Math.pow(2, binario.length() - 1 - i));
                     break;
-    
+
             }
         }
-    
+        this.resultado = decimal;
         return decimal;
     }
-    
-
-
 
     public static void main(String[] args) throws Exception {
         Calculadora conta = new Calculadora();
 
-        // conta.soma(5, 5);
-        // conta.sub(5, 5);
-        // conta.mult(5, 5);
-        // conta.div(5, 5);
-        // System.out.println(conta.getResultadoValor());
-        // System.out.println(conta.getResultadoString());
-        // System.out.println(9 / 2);
-        // System.out.println(9 % 2);
-        System.out.println(conta.convertDecToBin(9));
-        System.out.println(conta.convertBinToDec("1001"));
-        
+        conta.convertBinToDec("1010101");
+        System.out.println(conta.getResultadoString());
+        conta.convertDecToBin(85);
+        System.out.println(conta.getResultadoString());
 
     }
 }
---
+++ listaExercicios(17.09)/src/Retangulo.java
@@ -0,0 +1,22 @@
+public class Retangulo {
+    private double area, perimetro, lado;
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+
+
+}
