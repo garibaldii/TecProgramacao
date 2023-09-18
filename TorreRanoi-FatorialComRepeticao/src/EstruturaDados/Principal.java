f69b07ac507d49651e3aedd95a836e2a62b0c279
Matheus Garibaldi <125389052+garibaldii@users.noreply.github.com>
2023-09-18 17:05:48 -0300

    fatorialOk

---
+++ TorreRanoi-FatorialComRepeticao/src/EstruturaDados/Principal.java
@@ -0,0 +1,31 @@
+package EstruturaDados;
+
+public class Principal {
+
+
+    public static void main(String[] args) {
+
+        // ### FATORIAL ###
+
+        Fatorial calculoFatoracao = new Fatorial();
+        System.out.println(calculoFatoracao.recursaoFatorial(9));
+        System.out.println(calculoFatoracao.whileFatorial(9));
+        System.out.println(calculoFatoracao.forFatorial(9));
+
+
+        // ###
+
+
+        // ### TORRE DE HANOI ### 
+
+
+
+
+
+
+
+
+
+        // ###
+    }
+}
---
+++ TorreRanoi-FatorialComRepeticao/src/EstruturaDados/TorreHanoi.java
@@ -0,0 +1,10 @@
+package EstruturaDados;
+// Exercicio 2: Crie uma nova classe chamada TorreHanoi e implemente um método que imprima no console os passos para resolver o problema da Torre de Hanoi, dado um numero de discos definido pelo usuário. 
+
+public class TorreHanoi {
+    
+    public int jogoTorreHanoi(int quantidadeDiscos, char origem, char auxiliar, char destino){
+        
+
+    }
+}
